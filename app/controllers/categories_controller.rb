beaabd3f50b380df27a1f95b24ef34fd10e52e61
Mincho Praznikov <m.prajnikov@abv.bg>
2016-02-12 22:49:09 +0200

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
-  def index
-  end
-
-  def edit
-  end
-
-  def new
-  end
-
-  def show
-  end
-end
+	def index
+	@categories = Category.all
+	end
+	
+	def show
+	@category = Category.find(params[:id])
+	@title = @category.name
+	@posts = @category.posts
+	end
+	
+	private
+	def category_params
+		params.require(:category).permit(:name)		
+	end
+end 
