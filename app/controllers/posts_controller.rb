506f0b801b3c175008b82fa91e0e6aadb610e79c
Mincho Praznikov <m.prajnikov@abv.bg>
2016-02-12 13:47:58 +0200

    new posts added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,41 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController	
 	def index
-		@content1 = 'This is some sample text for my new Ruby blog';
-		@content2 = 'This is some more sample text for my new Ruby blog';
+		@posts = Post.all
 	end	
-	def new
-	
-	end	
-	def create
-	
-	end	
-	def edit	
-	
-	end	
-	def update	
-	end
-	
 	def show
-
+		@post = Post.find(params[:id])
 	end
+	def new
+		@post = Post.new
+		@category = Category.all
+	end
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path, :notice => "Your posts has been saved"
+		else
+			render "new"
+		end
+	end	
+	def edit
+		@post = Post.find(params[:id])
+	end				
+	def update	
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			redirect_to post_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
+	end	
 	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"
+	end
 	
+	private
+	def post_params
+		params.require(:post).permit(:title, :body, :category_id, :author_id)	
 	end
 end
